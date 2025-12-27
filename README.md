556487706434821e45f45b0e172225cdeb9dd7f9
Alex McDonald <alexm024680@gmail.com>
2025-12-27 02:54:25 -0700

    n-grams, probalistic orderpermute

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
 ## String Permutation & Utilities
 
-Example of how to guess a password of length 12 given the keyboard keys used are: {P, I, M, S, 1, 7}:
+Example of how to guess a word of length 5 given the letters used are: {Z, A, P, I}:
 
 ```
-echo "pims127" | ./lenextender 12 | ./charpermute | ./orderpermute
+echo "zapi" | ./lenextender 5 | ./orderpermute | grep "pizza"
 ```
 
 Example of how to guess a 12 character password given the letter keys pressed on a phone keypad are: {1, 4, 6, 7}:
---
+++ utils.py
@@ -0,0 +1,30 @@
+
+def isArgFlag(arg: str):
+    return (arg.startswith("--") and len(arg) > 2) or (arg.startswith("-") and len(arg) > 1)
+
+def isArg2Flag(arg: str):
+    return (arg.startswith("--") and len(arg) > 2)
+
+def getArgFlag(arg: str):
+    return arg[2:] if arg.startswith('--') else arg[1:]
+
+def getFlags(args):
+    mainArgs = []
+    flags = set()
+    vals = {}
+
+    prevWasArg = False
+    for i, arg in enumerate(args):
+        if (isArgFlag(arg)):
+            arg_orig = arg
+            arg = getArgFlag(arg)
+            flags.add(arg)
+
+            if (i < len(args)-1) and not isArgFlag(args[i+1]):
+                vals[arg] = args[i+1]
+            prevWasArg = isArg2Flag(arg_orig)
+        else: 
+            if (not prevWasArg): mainArgs.append(arg)
+            prevWasArg = False
+
+    return flags, vals, mainArgs
